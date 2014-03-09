919c5e57bde550e286336f2b5260b42c5535225a
Andrew Petersen <kirbysayshi@gmail.com>
2014-03-09 19:13:37 -0400

    whitespace fixes. ALL MAIN TESTS PASSING

--- test/vows/vash.test.js
+++ test/vows/vash.test.js
@@ -1134,7 +1134,7 @@ vows.describe('vash templating library').addBatch({
 			return vash.compile(str);
 		}
 		,'renders': function(topic){
-			assert.equal( topic(), '<div class="how"> <div class="item-0">I be an item!</div></div>' );
+			assert.equal( topic(), '<div class="how"> <div class="item-0">I be an item!</div> </div>' );
 		}
 	}
 	,'unclosed block': {
@@ -1202,10 +1202,10 @@ vows.describe('vash templating library').addBatch({
 			topic: '<span>@model<span\n></span></span>'
 
 			,'does not prevent HTML matching': function(topic) {
-				var tpl = vash.compile(topic)
+				var tpl = vash.compile(topic, {useWith: false})
 					, actual = tpl('1');
 
-				assert.equal( '<span>1<span\n></span></span>', actual )
+				assert.equal( '<span>1<span></span></span>', actual )
 			}
 		}
 
@@ -1239,7 +1239,7 @@ vows.describe('vash templating library').addBatch({
 			assert.doesNotThrow( function(){ vash.compile(topic) }, Error );
 		}
 		,'can be called': function(topic){
-			assert.equal( vash.compile(topic)(), 'what' );
+			assert.equal( vash.compile(topic)(), ' what' );
 		}
 	}
 	,'@function with markup': {
@@ -1248,7 +1248,7 @@ vows.describe('vash templating library').addBatch({
 			assert.doesNotThrow( function(){ vash.compile(topic) }, Error );
 		}
 		,'can be called': function(topic){
-			assert.equal( vash.compile(topic)({ name: 'what' }), '<li>what</li>' );
+			assert.equal( vash.compile(topic)({ name: 'what' }), ' <li>what</li>' );
 		}
 	}
 
